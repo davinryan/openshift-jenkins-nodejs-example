9248b1bb7e093a2fb4c1ef1d264490519c5844f3
766347 <Dig6Hef6>
2018-01-03 10:01:01 +1300

    Added health check

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => res.send('Hello World!'));
-app.get('/health', (req, res) => res.send('Hello World is healthy!'));
+console.log('Successfully registered path /');
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.get('/health', (req, res) => res.send('Hello World is healthy!'));
+console.log('Successfully registered path /health');
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
