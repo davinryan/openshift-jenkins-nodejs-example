78fefa8116ef53fde43860af6cc54c16066bca70
766347 <Dig6Hef6>
2018-01-03 09:53:25 +1300

    Added health check

--- src/app.js
+++ src/app.js
@@ -2,5 +2,6 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/health', (req, res) => res.send('Hello World is healthy!'));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
