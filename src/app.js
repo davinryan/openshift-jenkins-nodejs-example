2ad1a09354bebe034fbada8d44d2de520256d171
766347 <Dig6Hef6>
2018-01-03 10:45:10 +1300

    Updated health checks and readiness probe

--- src/app.js
+++ src/app.js
@@ -7,7 +7,4 @@ console.log('Successfully registered path /');
 app.get('/health', (req, res) => res.send('Hello World is healthy!'));
 console.log('Successfully registered path /health');
 
-app.get('/healthz', (req, res) => res.send('Hello World is healthyz!'));
-console.log('Successfully registered path /healthz');
-
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
