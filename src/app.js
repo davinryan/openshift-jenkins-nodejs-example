3a74c1859de24b03d1e34e3e88952b4edd548e65
766347 <Dig6Hef6>
2018-01-06 21:48:06 +1300

    Updated config files

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const app = express();
 app.get('/', (req, res) => res.send('Hello World!'));
 console.log('Successfully registered path /');
 
-app.get('/health', (req, res) => res.send('Hello World is healthy!'));
+app.get('/health', (req, res) => res.send('Hello World is healthy! I\'m running in ' + process.env.ENVIRONMENT));
 console.log('Successfully registered path /health');
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
